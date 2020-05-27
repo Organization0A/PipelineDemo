faf888b69ceb206f782b567fadab08256868c99e
wanarenikhil <nikhilwanare@gmail.com>
2020-05-27 12:02:30 +0530

    Init commit

---
+++ WebApplication1/WebApplication1Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            var controller = new HomeController();
+
+            var expected = "Hello pipeline";
+
+            var actual = controller.Index();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
